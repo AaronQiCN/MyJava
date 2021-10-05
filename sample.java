733f6ca7fac7421ae7872b55bff955b80126673d
AaronQiCN <aaronqk2021@163.com>
2021-10-05 10:50:31 +0000

    Add content to sample.

--- sample.java
+++ sample.java
@@ -6,4 +6,9 @@
  ************************************************************************/
 
 public class sample
-
+{
+    public static void main(String args)
+    {
+        System.out.println("Hello World!");
+    }
+}
