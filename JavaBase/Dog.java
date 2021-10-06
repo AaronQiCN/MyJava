7600054f28aef117623e685128f5ae5435e0b83d
AaronQiCN <aaronqk2021@163.com>
2021-10-06 06:45:10 +0000

    create a new file from OnePlus 8.

---
+++ JavaBase/Dog.java
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: Dog.java
+	> Author: 
+	> Mail: 
+	> Created Time: Wed Oct  6 06:42:04 2021
+ ************************************************************************/
+
+public class Dog
+{    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
